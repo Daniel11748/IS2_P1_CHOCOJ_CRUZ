ac214bcda8e7ee09a4f9690ed319bec666f9d6f9
CHOCOJ <daniel239cruz@gmail.com>
2024-07-15 08:31:32 -0600

    se agregan eventos para que se reconozcan cada uno de los elementos del formulario y se corrige un error en el index

---
+++ sccript.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const productoForm = document.getElementById('productoForm');
+    const productoNombre = document.getElementById('productNombre');
+    const productoPrecio = document.getElementById('productPrecio');
+    const productoTable = document.getElementById('productTable').getElementsByTagName('tbody')[0];
+    const totalComprado = document.getElementById('totalComprado');
+
+    let total = 0;
+
+ 
+});
